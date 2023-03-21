d2fefeda7f1885b18332efddefb7c261088ce4aa
chajus1 <chajus1@googlemail.com>
2023-03-21 08:36:55 +0000

    Add project files.

---
+++ CalculatingWaterPerDay/Calculating.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingWaterPerDay
+{
+    internal class Calculating
+    {
+        // Math 
+        // 1oz = 0.02835kg
+        // 1cup = 236.5882ml
+        // 1lbs = 0.453592kg
+        //
+        // .5oz/1lbs of body weight
+        //
+
+
+        public static int CalculatingWeight()
+        {
+
+            return 0;
+
+
+        }
+
+        public static void ClassicRuleInfo()
+        {
+            Console.WriteLine("Classic Rule is 0.5oz/1lbs of body weight.\n");
+        }
+
+    }
+}
---
+++ CalculatingWaterPerDay/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingWaterPerDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Calculating.ClassicRuleInfo();
+            Console.WriteLine($"Would you like to make the calculation:\n1.Kilograms (metric) \n2.Pounds (imperial)\n");
+            
+                choice = Char.ToUpper(Console.ReadKey().KeyChar);
+                //int num = int.TryParse(choice, out int bob);
+                switch (choice)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("\n u stuped");
+                        return;
+                }
+            
+                // switch 
+                // metric
+                // or
+                // imperial
+
+                Console.WriteLine("Hello.\nHow much do you weigh?\n");
+
+                //Calculation
+
+                Console.WriteLine("Would you like to convert the number to alternative system?\n (Y or N)");
+
+                // switch 
+                // convert and bye
+                // or just bye
+            
+        }
+    }
+}
